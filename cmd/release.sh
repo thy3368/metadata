8864a27ea6129521eff179bde91c3cb4214846aa
Sergei Visotsky <sergei.visotsky@gmail.com>
2022-01-23 21:23:55 +0100

    update release.sh

--- cmd/release.sh
+++ cmd/release.sh
@@ -13,6 +13,17 @@ if [ "$1" == "version" ]; then
   git checkout master
 fi
 
+if [ "$1" == "patch" ]; then
+  mvn clean install
+  mvn deploy
+  git add .
+  git commit -m "release $2"
+  git tag v"$2"
+  git push
+  git push origin v"$2"
+fi
+
+
 if [ "$1" == "snapshot" ]; then
   mvn clean install
   mvn deploy
