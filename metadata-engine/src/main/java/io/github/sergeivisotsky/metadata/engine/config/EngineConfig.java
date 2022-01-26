0db1062d84bc4670a606562fc87ec75e87c4c22f
Sergei Visotsky <sergei.visotsky@gmail.com>
2022-01-26 19:38:50 +0100

    #89 exclude not needed Spring Boot auto-configuration

--- metadata-engine/src/main/java/io/github/sergeivisotsky/metadata/engine/config/EngineConfig.java
+++ metadata-engine/src/main/java/io/github/sergeivisotsky/metadata/engine/config/EngineConfig.java
@@ -25,5 +25,5 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
  */
 @Configuration
 @ConditionalOnMissingBean(annotation = ControllerAdvice.class)
-public class SelectorConfig {
+public class EngineConfig {
 }
